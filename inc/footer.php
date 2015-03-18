3b952ef3240e484ad84a96fca1e0609124f8d639
Turgittin <turgittin_mac@Turgittin-MacBook-Pro.local>
2015-03-19 00:40:12 +0200

    top revize!

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <div id="outer_footer">
     <div id="footer">
       <div class="bottom_footer">
-        <p>Copyright &copy; 2011 Gökçe Pehlivanoğlu. All rights reserved.</p>
+        <p>Copyright &copy; 2015 Gökçe Pehlivanoğlu. All rights reserved.</p>
         <a href="http://www.turgayozturk.com.tr" target="_blank"><a href="http://www.turgayozturk.com.tr" target="_blank"><img src="./images/turgayozturk-design-studio.png" border="0" /></a></div>
     </div>
   </div>
--- music-videos.php
+++ music-videos.php
@@ -13,11 +13,10 @@
   <div id="wrapper">
         <!--header -->
     <div id="header"> <a href="/"><img src="./images/logo2.png" alt="" id="logo" /></a>
-      <br /><br /><br /><br /><br /><div id="right_header">
-        
+      <div id="right_header">     
         <div>
           <ul>
-            <li class="style1">
+            <li class="topsosyalmedya">
               <div align="right"><a href="http://www.facebook.com/pages/Lunaroom-Film/142076982475137" target="_blank"><img src="images/facebook.png" alt="" /></a>&nbsp;<a href="http://twitter.com/#!/GokcePehlvnoglu" target="_blank"><img src="images/twitter.png" alt="" /></a>&nbsp;<a href="http://www.myspace.com/gokcepehlivanoglu" target="_blank"><img src="images/myspace.png" alt="" /></a>&nbsp;<a href="http://anahita.deviantart.com/" target="_blank"><img src="images/devi.png" alt="" /></a>&nbsp;<a href="http://vimeo.com/gokcepehlivanoglu" target="_blank"><img src="images/vimeo.png" alt="" /></a></div>
             </li>
             <li></li>
