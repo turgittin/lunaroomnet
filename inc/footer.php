6aa8690de5cc0062df4e974b520843cea26e4456
Erdi <erdi.doqan@gmail.com>
2015-03-19 01:14:21 +0100

    kucuk resimler siyah beyaz ve activeler olusturuldu

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
     <div id="footer">
       <div class="bottom_footer">
         <p>Copyright &copy; 2015 Gökçe Pehlivanoğlu. All rights reserved.</p>
-        <a href="http://www.turgayozturk.com.tr" target="_blank"><a href="http://www.turgayozturk.com.tr" target="_blank"><img src="./images/turgayozturk-design-studio.png" border="0" /></a></div>
+        <a href="http://www.pyro.com.tr/" target="_blank"><strong>&nbsp;&nbsp; &nbsp;<img src="../images/pyro.png" style="float:right;"></strong></a>
     </div>
   </div>
   <br class="clear" />
